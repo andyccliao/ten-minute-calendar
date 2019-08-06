119c92838b69389295daad9309be8b3f009b1cdd
andyccliao <andyccliao@gmail.com>
2019-08-06 10:18:31 -0700

    Webpage shows up.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,86 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const rows = 12;
+const cols = 12;
+
+function Square(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
   );
+}
+
+class Grid extends React.Component {
+  renderSquare(i) {
+    return <Square 
+      value={this.props.grid[i]} 
+      onClick={() => this.props.onClick(i)}
+    />;
+  }
+
+  renderRow(m, n) {
+    const row = this.props.grid.slice(m*n, (m+1)*n);
+
+    return (
+      <tr>
+        <th>{(2*m+5)%12}</th>
+        {row.map( 
+          (value, index) => {
+            return (<td>
+              {this.renderSquare(m*n + index)}
+            </td>)
+          }
+        )}
+      </tr>
+    );
+  }
+
+  renderTable() {
+    const tbheadrow = (
+      <tr>
+        <th/><th/><th/><th/><th/><th/><th/><th>|</th><th/><th/><th/><th/><th/>
+      </tr>
+    );
+
+    let tbrows = [];
+    for (let i = 0; i < rows; i++) {
+      tbrows.push(this.renderRow(i, cols));
+    }
+
+    return (
+      <table>
+        {tbheadrow}
+        {tbrows}
+      </table>
+    );
+  }
+
+  render() {
+    return this.renderTable()
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      grid: Array(144).fill(null)
+    }
+  }
+
+  render() { 
+    return (
+      <div className="App">
+        <div className="Grid">
+          <Grid
+            grid={this.state.grid}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
